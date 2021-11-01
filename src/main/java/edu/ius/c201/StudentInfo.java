202baac17e37d58e28e4f9679225eef93099491e
Chris Sexton <chrissexton@users.noreply.github.com>
2021-11-01 10:43:38 -0400

    student version

--- src/main/java/edu/ius/c201/StudentInfo.java
+++ src/main/java/edu/ius/c201/StudentInfo.java
@@ -1,69 +1,21 @@
 package edu.ius.c201;
 
-public class StudentInfo {
-    private double maxGrade;
+/**
+ * CSU Lab 09 - Arrays
+ * https://www.cs.colostate.edu/~cs163/.Fall20/001/labs/lab09Arrays
+ *
+ * Be sure to look at StudentInfoTest.java in the test directory.
+ *
+ * You will want to name your methods the same as that file expects and the lab indicates.
+ */
 
-    private double[] grades;
-    private String[] names;
+public class StudentInfo {
+    // Private members should go here
 
     public StudentInfo(double maxGrade, int totalStudents) {
-        this.grades = new double[totalStudents];
-        this.names = new String[totalStudents];
-        this.maxGrade = maxGrade;
     }
 
-    public double getGrade(int id) {
-        return grades[id];
-    }
+    // Public getters and setters
 
-    public String getName(int id) {
-        return names[id];
-    }
-
-    public void setGrade(int id, double newGrade) {
-        grades[id] = newGrade;
-    }
-
-    public void setName(int id, String newName) {
-        names[id] = newName;
-    }
-
-    public double getGradeByName(String name) {
-        for (int i = 0; i < grades.length; i++) {
-            if (names[i] == name) {
-                return grades[i];
-            }
-        }
-        return -1;
-    }
-
-    public void setGradeByName(String name, double grade) {
-        for (int i = 0; i < grades.length; i++) {
-            if (names[i] == name) {
-                grades[i] = grade;
-            }
-        }
-    }
-
-    public int numBestGrades() {
-        int count = 0;
-        for (int i = 0; i < grades.length; i++) {
-            if (grades[i] >= 50 && grades[i] <= maxGrade) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    public String[] findBestStudents() {
-        String[] best = new String[numBestGrades()];
-        int j = 0;
-        for (int i = 0; i < grades.length; i++) {
-            if (grades[i] >= 50 && grades[i] <= maxGrade) {
-                best[j] = names[i];
-                j++;
-            }
-        }
-        return best;
-    }
+    // Public methods
 }
--- src/test/java/edu/ius/c201/StudentInfoTest.java
+++ src/test/java/edu/ius/c201/StudentInfoTest.java
@@ -1,71 +1,83 @@
 package edu.ius.c201;
 
+/**
+ * CSU Lab 09 - Arrays
+ * https://www.cs.colostate.edu/~cs163/.Fall20/001/labs/lab09Arrays
+ *
+ * Be sure to uncomment and run tests each time you have completed a method!
+ *
+ * Uncomment the @Test line along with the entire test method. If a test does not pass
+ * or cannot be compiled (IDE won't run it), you must fix the problem before continuing.
+ *
+ * Be sure to name your methods the same as they are called in this file.
+ */
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class StudentInfoTest {
-    @Test
-    public void getAndSetGrade() {
-        double expected = 50;
-        StudentInfo s = new StudentInfo(100.0, 10);
-        s.setGrade(0, 50);
-        double actual = s.getGrade(0);
-        assertEquals(expected, actual, 0.01);
-    }
-
-    @Test
-    public void getAndSetName() {
-        String expected = "foo";
-        StudentInfo s = new StudentInfo(100.0, 10);
-        s.setName(0, expected);
-        String actual = s.getName(0);
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void getGradeByName() {
-        double expected = 10.5;
-        String name = "foo";
-        StudentInfo s = new StudentInfo(100.0, 10);
-        s.setName(8, "foo");
-        s.setGrade(8, expected);
-        double actual = s.getGradeByName("foo");
-        assertEquals(expected, actual, 0.01);
-    }
-
-    @Test
-    public void setGradeByName() {
-        double expected = 65.5;
-        String name = "foo";
-        StudentInfo s = new StudentInfo(100.0, 10);
-        s.setName(5, name);
-        s.setGradeByName(name, expected);
-        double actual = s.getGrade(5);
-        assertEquals(expected, actual, 0.01);
-    }
-
-    @Test
-    public void numBestGrades() {
-        int expected = 5;
-        StudentInfo s = new StudentInfo(100.0, 10);
-        for (int i = 0; i < 10; i++) {
-            s.setName(i, "Student"+i);
-            s.setGrade(i, i*10.0);
-        }
-        int actual = s.numBestGrades();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void findBestStudents() {
-        String[] expected = {"Student5","Student6","Student7","Student8","Student9"};
-        StudentInfo s = new StudentInfo(100.0, 10);
-        for (int i = 0; i < 10; i++) {
-            s.setName(i, "Student"+i);
-            s.setGrade(i, i*10.0);
-        }
-        String[] actual = s.findBestStudents();
-        assertArrayEquals(expected, actual);
-    }
+//    @Test
+//    public void getAndSetGrade() {
+//        double expected = 50;
+//        StudentInfo s = new StudentInfo(100.0, 10);
+//        s.setGrade(0, 50);
+//        double actual = s.getGrade(0);
+//        assertEquals(expected, actual, 0.01);
+//    }
+//
+//    @Test
+//    public void getAndSetName() {
+//        String expected = "foo";
+//        StudentInfo s = new StudentInfo(100.0, 10);
+//        s.setName(0, expected);
+//        String actual = s.getName(0);
+//        assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void getGradeByName() {
+//        double expected = 10.5;
+//        String name = "foo";
+//        StudentInfo s = new StudentInfo(100.0, 10);
+//        s.setName(8, "foo");
+//        s.setGrade(8, expected);
+//        double actual = s.getGradeByName("foo");
+//        assertEquals(expected, actual, 0.01);
+//    }
+//
+//    @Test
+//    public void setGradeByName() {
+//        double expected = 65.5;
+//        String name = "foo";
+//        StudentInfo s = new StudentInfo(100.0, 10);
+//        s.setName(5, name);
+//        s.setGradeByName(name, expected);
+//        double actual = s.getGrade(5);
+//        assertEquals(expected, actual, 0.01);
+//    }
+//
+//    @Test
+//    public void numBestGrades() {
+//        int expected = 5;
+//        StudentInfo s = new StudentInfo(100.0, 10);
+//        for (int i = 0; i < 10; i++) {
+//            s.setName(i, "Student"+i);
+//            s.setGrade(i, i*10.0);
+//        }
+//        int actual = s.numBestGrades();
+//        assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void findBestStudents() {
+//        String[] expected = {"Student5","Student6","Student7","Student8","Student9"};
+//        StudentInfo s = new StudentInfo(100.0, 10);
+//        for (int i = 0; i < 10; i++) {
+//            s.setName(i, "Student"+i);
+//            s.setGrade(i, i*10.0);
+//        }
+//        String[] actual = s.findBestStudents();
+//        assertArrayEquals(expected, actual);
+//    }
 }
